c47886a6ab4966d973a1939665f638a3ef30673d
tkote <7146253+tkote@users.noreply.github.com>
2020-03-27 12:55:15 +0900

    added -v option with commns-cli

--- src/main/java/cf/ocha/App.java
+++ src/main/java/cf/ocha/App.java
@@ -1,5 +1,15 @@
 package cf.ocha;
 
+import java.io.File;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 /**
  * Hello world!
  *
@@ -8,9 +18,29 @@ public class App
 {
     public static void main( String[] args )
     {
-        final String javaHome = System.getProperty("java.home").replaceAll("/jre$", "");
-        System.err.println(String.format("java.home=%s", javaHome));
+        Options options = new Options();
+        Option vOpt = Option.builder("v")
+            .longOpt("verbose")
+            .hasArg(false)
+            .desc("verbose mode")
+            .build();
+        options.addOption(vOpt);
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = null;
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.err.println(e.getMessage());
+            HelpFormatter hf = new HelpFormatter();
+            hf.printHelp("[options]", options);
+            System.exit(1);
+        }
+
+        final boolean verbose = cmd.hasOption("v");
+
+        final String javaHome = System.getProperty("java.home").replaceAll(File.separator + "jre$", "");
+        if(verbose) System.err.println(String.format("java.home=%s", javaHome));
         System.out.print(javaHome);
-        System.err.println();
+        if(verbose) System.err.println();
     }
 }
